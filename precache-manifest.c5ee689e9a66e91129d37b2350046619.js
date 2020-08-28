bd5a29b693133fd065f848b6d7c9ae428b4dd25d
Neil Hughes <enhughesiasm@googlemail.com>
2020-08-28 10:28:47 +0100

    Updates

---
+++ precache-manifest.c5ee689e9a66e91129d37b2350046619.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f08a332670ee07a97b4ba487c90fa05b",
+    "url": "/q/index.html"
+  },
+  {
+    "revision": "aaf132f0507ea448d815",
+    "url": "/q/static/css/2.10b2f6bd.chunk.css"
+  },
+  {
+    "revision": "1589060dcd8ee42fad88",
+    "url": "/q/static/css/main.d6ab441a.chunk.css"
+  },
+  {
+    "revision": "aaf132f0507ea448d815",
+    "url": "/q/static/js/2.d276ae26.chunk.js"
+  },
+  {
+    "revision": "45aee433f980db42e5983aa48150e7c0",
+    "url": "/q/static/js/2.d276ae26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1589060dcd8ee42fad88",
+    "url": "/q/static/js/main.a2b5a9e3.chunk.js"
+  },
+  {
+    "revision": "1a53dc21252f9b5e38e7",
+    "url": "/q/static/js/runtime-main.7f20f8ab.js"
+  }
+]);
