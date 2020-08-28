42d6b6c729dbcfb9e82a61f7cfee821a1661d9cd
Neil Hughes <enhughesiasm@googlemail.com>
2020-08-28 11:05:06 +0100

    Fixed routing for gh-pages

--- src/App.js
+++ src/App.js
@@ -6,13 +6,30 @@ import Quiz from './components/quiz/quiz';
 import AdminGate from './components/admin/admin_gate';
 import Scores from './components/scores/scores';
 
+console.log(process.env.PUBLIC_URL);
+
 const App = () => (
 	<ErrorBoundary>
-		<BrowserRouter>
+		<BrowserRouter basename='/q'>
 			<Switch>
-				<Route exact path='/' component={Quiz} />
-				<Route exact path='/admin' component={AdminGate} />
-				<Route exact path='/scores' component={Scores} />
+				<Route
+					exact
+					path='/'
+					// path={`${process.env.PUBLIC_URL}/q/`}
+					component={Quiz}
+				/>
+				<Route
+					exact
+					path={'/admin'}
+					// path={`${process.env.PUBLIC_URL}/admin`}
+					component={AdminGate}
+				/>
+				<Route
+					exact
+					path={'/scores'}
+					// path={`${process.env.PUBLIC_URL}/scores`}
+					component={Scores}
+				/>
 				<Route component={NotFound} />
 			</Switch>
 		</BrowserRouter>
